20b3c8a10a8951f98004e2b9924ed33d992b25e5
Abdiel Lopez <48071553+PaperPrototype@users.noreply.github.com>
2025-12-09 21:05:58 -0500

    updated to use express websockets

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,24 +1,60 @@
-import { WebSocketServer } from 'ws'
+// import { WebSocketServer } from 'ws'
+// import * as rpc from 'vscode-ws-jsonrpc'
+// import { launch } from './launch'
+
+// // If you change the port, make sure to also change it for the client!
+// // const port = 8080
+
+// // @ts
+// const PORT: number = parseInt(<string>process.env.PORT, 10) || 8080
+
+
+// new WebSocketServer({ port: PORT }).on('connection', (webSocket) => {
+//     const socket: rpc.IWebSocket = {
+//         send: (content) =>
+//             webSocket.send(content, (error) => {
+//                 if (error) throw error
+//             }),
+//         onMessage: (callback) => webSocket.on('message', callback),
+//         onError: (callback) => webSocket.on('error', callback),
+//         onClose: (callback) => webSocket.on('close', callback),
+//         dispose: () => webSocket.close(),
+//     }
+//     if (webSocket.readyState === webSocket.OPEN) {
+//         launch(socket)
+//     } else {
+//         webSocket.on('open', () => launch(socket))
+//     }
+// })
+import express = require('express')
+import expressWs = require('express-ws')
 import * as rpc from 'vscode-ws-jsonrpc'
 import { launch } from './launch'
 
-// If you change the port, make sure to also change it for the client!
-const port = 8080
+const PORT: number = parseInt(process.env.PORT as string, 10) || 8080
 
-new WebSocketServer({ port }).on('connection', (webSocket) => {
+const app = express()
+const wsInstance = expressWs(app)
+
+wsInstance.app.ws('/', (ws, req) => {
     const socket: rpc.IWebSocket = {
         send: (content) =>
-            webSocket.send(content, (error) => {
+            ws.send(content, (error) => {
                 if (error) throw error
             }),
-        onMessage: (callback) => webSocket.on('message', callback),
-        onError: (callback) => webSocket.on('error', callback),
-        onClose: (callback) => webSocket.on('close', callback),
-        dispose: () => webSocket.close(),
+        onMessage: (callback) => ws.on('message', callback),
+        onError: (callback) => ws.on('error', callback),
+        onClose: (callback) => ws.on('close', callback),
+        dispose: () => ws.close(),
     }
-    if (webSocket.readyState === webSocket.OPEN) {
+    
+    if (ws.readyState === ws.OPEN) {
         launch(socket)
     } else {
-        webSocket.on('open', () => launch(socket))
+        ws.on('open', () => launch(socket))
     }
+})
+
+app.listen(PORT, () => {
+    console.log(`WebSocket server listening on port ${PORT}`)
 })
